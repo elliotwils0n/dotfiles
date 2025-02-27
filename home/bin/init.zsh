7a07ca129b216149d88238b0ff6775eb277327f8
elliotwils0n <ewils0n@protonmail.ch>
2025-02-27 20:53:55 +0100

    zsh: add fuzzy completion

--- home/bin/init.zsh
+++ home/bin/init.zsh
@@ -39,6 +39,7 @@ select-word-style bash
 
 # Completion
 zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 # Colors
