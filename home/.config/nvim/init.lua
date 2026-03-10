0ab36f8124b1648b856fe0e22b69c289eb6bfefb
elliotwils0n <ewils0n@protonmail.ch>
2026-03-10 17:45:50 +0100

    update

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -161,6 +161,7 @@ require("blink.cmp").setup({})
 require("lint").linters_by_ft = {
   rust = { "clippy" },
   go = { "golangcilint" },
+  python = { "ruff" },
 }
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   callback = function()
@@ -334,5 +335,48 @@ dap.configurations.go = {
     request = "launch",
     mode = "test",
     program = "./${relativeFileDirname}",
+  },
+}
+
+-- DAP Python
+dap.adapters.python = function(cb, config)
+  if config.request == "attach" then
+    local port = (config.connect or config).port
+    local host = (config.connect or config).host or "127.0.0.1"
+    cb({
+      type = "server",
+      port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+      host = host,
+      options = {
+        source_filetype = "python",
+      },
+    })
+  else
+    cb({
+      type = "executable",
+      command = "/usr/bin/python",
+      args = { "-m", "debugpy.adapter" },
+      options = {
+        source_filetype = "python",
+      },
+    })
+  end
+end
+dap.configurations.python = {
+  {
+    type = "python",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    pythonPath = function()
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+        return cwd .. "/venv/bin/python"
+      elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+        return cwd .. "/.venv/bin/python"
+      else
+        return "/usr/bin/python"
+      end
+    end,
   },
 }
